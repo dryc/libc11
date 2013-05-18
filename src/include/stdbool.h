c49440122f4cbd55c6ef3dae251c172a537f858f
Arto Bendiken <arto@bendiken.net>
2013-05-18 23:23:52 +0200

    Defined the <stddef.h> and <stdbool.h> header files.

---
+++ src/include/stdbool.h
@@ -0,0 +1,12 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef _STDBOOL_H
+#define _STDBOOL_H
+
+/**
+ * @file
+ *
+ * <stdbool.h> - Boolean type and values.
+ */
+
+#endif /* _STDBOOL_H */
---
+++ src/include/stddef.h
@@ -0,0 +1,12 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef _STDDEF_H
+#define _STDDEF_H
+
+/**
+ * @file
+ *
+ * <stddef.h> - Common definitions.
+ */
+
+#endif /* _STDDEF_H */
