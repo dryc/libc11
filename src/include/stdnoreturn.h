21b65e9c1d13a441771e6a1224aec4f6675cd66f
Arto Bendiken <arto@bendiken.net>
2013-05-19 00:15:52 +0200

    Implemented the <stdnoreturn.h> header per the C11 standard.

---
+++ src/include/stdnoreturn.h
@@ -0,0 +1,16 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef _STDNORETURN_H
+#define _STDNORETURN_H
+
+/**
+ * @file
+ *
+ * <stdnoreturn.h> - _Noreturn.
+ */
+
+#ifndef __cplusplus
+#define noreturn _Noreturn
+#endif
+
+#endif /* _STDNORETURN_H */
