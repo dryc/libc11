3e638b04d3e6b14c7494af060ec4b0944efd0cd5
Arto Bendiken <arto@bendiken.net>
2013-05-19 00:01:56 +0200

    Defined the <inttypes.h> header file from the C11 standard.

---
+++ src/include/inttypes.h
@@ -0,0 +1,14 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef _INTTYPES_H
+#define _INTTYPES_H
+
+/**
+ * @file
+ *
+ * <inttypes.h> - Format conversion of integer types.
+ */
+
+#include <stdint.h>
+
+#endif /* _INTTYPES_H */
