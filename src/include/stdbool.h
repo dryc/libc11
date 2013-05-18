92b0c33abd3b7dded9cffd13fc55d780d3f3dfda
Arto Bendiken <arto@bendiken.net>
2013-05-18 23:35:50 +0200

    Implemented the <stdbool.h> macros per the C11 standard.

--- src/include/stdbool.h
+++ src/include/stdbool.h
@@ -7,6 +7,16 @@
  * @file
  *
  * <stdbool.h> - Boolean type and values.
+ *
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/stdbool.h.html
  */
+
+#ifndef __cplusplus
+#define bool  _Bool
+#define true  1
+#define false 0
+#endif
+
+#define __bool_true_false_are_defined 1
 
 #endif /* _STDBOOL_H */
