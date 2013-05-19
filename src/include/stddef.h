44075599053fc87be29542301cfdceef2ac6b45b
Arto Bendiken <arto@bendiken.net>
2013-05-19 02:15:58 +0200

    Defined the size_t and ptrdiff_t types in the <stddef.h> header.

--- src/include/stddef.h
+++ src/include/stddef.h
@@ -11,4 +11,14 @@
  * @see http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/stddef.h.html
  */
 
+/**
+ * The signed integer type of the result of subtracting two pointers.
+ */
+typedef signed long ptrdiff_t;
+
+/**
+ * The unsigned integer type of the result of the `sizeof` operator.
+ */
+typedef unsigned long size_t;
+
 #endif /* _STDDEF_H */
