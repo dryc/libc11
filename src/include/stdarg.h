8e445e5ade0947f97895a5b2830a3d7ca4de293a
Arto Bendiken <arto@bendiken.net>
2013-05-19 02:02:02 +0200

    Implemented the <stdarg.h> header per the C11 standard.
    Note: presently requires a compiler with __builtin_va_*() primitives.

--- src/include/stdarg.h
+++ src/include/stdarg.h
@@ -11,4 +11,14 @@
  * @see http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/stdarg.h.html
  */
 
+typedef __builtin_va_list va_list;
+
+#define va_start(ap, param) __builtin_va_start((ap), (param))
+
+#define va_arg(ap, type)    __builtin_va_arg((ap), (type))
+
+#define va_end(ap)          __builtin_va_end((ap))
+
+#define va_copy(dest, src)  __builtin_va_copy((dest), (src))
+
 #endif /* _STDARG_H */
