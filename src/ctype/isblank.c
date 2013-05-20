c76350ac5d8822b89454ce2518b637f231fe04d4
Arto Bendiken <arto@bendiken.net>
2013-05-20 03:18:59 +0200

    Defined the <ctype.h> is*() predicate functions from C11.

---
+++ src/ctype/isblank.c
@@ -0,0 +1,12 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <ctype.h>
+
+int
+isblank(const int c) {
+  return (void)c, 0; // TODO
+}
--- src/include/ctype.h
+++ src/include/ctype.h
@@ -15,6 +15,34 @@
 extern "C" {
 #endif
 
+int isalnum(int c);
+
+int isalpha(int c);
+
+#if defined(_BSD_SOURCE) || defined(_SVID_SOURCE) || defined(_XOPEN_SOURCE)
+int isascii(int c);
+#endif
+
+int isblank(int c);
+
+int iscntrl(int c);
+
+int isdigit(int c);
+
+int isgraph(int c);
+
+int islower(int c);
+
+int isprint(int c);
+
+int ispunct(int c);
+
+int isspace(int c);
+
+int isupper(int c);
+
+int isxdigit(int c);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
