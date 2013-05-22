9461e5166a5a2ddd93995aab58442cdc67fd073f
Arto Bendiken <arto@bendiken.net>
2013-05-22 04:07:31 +0200

    Defined the <locale.h> and <signal.h> headers from C11.

---
+++ src/include/locale.h
@@ -0,0 +1,14 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef _LOCALE_H
+#define _LOCALE_H
+
+/**
+ * @file
+ *
+ * <locale.h> - C11 7.11: Localization.
+ *
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/locale.h.html
+ */
+
+#endif /* _LOCALE_H */
---
+++ src/include/signal.h
@@ -0,0 +1,14 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef _SIGNAL_H
+#define _SIGNAL_H
+
+/**
+ * @file
+ *
+ * <signal.h> - C11 7.14: Signal handling.
+ *
+ * @see http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/signal.h.html
+ */
+
+#endif /* _SIGNAL_H */
