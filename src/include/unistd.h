e8d493ec2418214d94a131f5b27319fc2f990e74
Arto Bendiken <arto@bendiken.net>
2013-05-26 08:43:54 +0200

    Defined the <unistd.h> header from POSIX.1-2008.

---
+++ src/include/unistd.h
@@ -0,0 +1,22 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef _UNISTD_H
+#define _UNISTD_H
+
+/**
+ * @file
+ *
+ * <unistd.h> - POSIX.1-2008.
+ *
+ * @see http://libc11.org/unistd/
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _UNISTD_H */
