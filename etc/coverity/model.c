6d8cb1f3da7daf24997560601b5e9df473e805eb
Arto Bendiken <arto@bendiken.net>
2014-10-11 15:30:52 +0200

    Added a Coverity Scan model file placeholder.

---
+++ etc/coverity/model.c
@@ -0,0 +1,9 @@
+/* This is free and unencumbered software released into the public domain. */
+
+/**
+ * @file
+ *
+ * Coverity Scan model for libc11.
+ *
+ * @see https://scan.coverity.com/tune
+ */
