6cb71abd45f3d6712197917e9361fddf980b3cd9
Arto Bendiken <arto@bendiken.net>
2014-11-14 21:10:55 +0100

    Implemented the classic "Hello, world!" example program.

---
+++ doc/examples/hello/hello.c
@@ -0,0 +1,11 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include <stdio.h>  /* for puts() */
+#include <stdlib.h> /* for EXIT_SUCCESS */
+
+int main(int argc, char* argv[]) {
+
+  puts("Hello, world!");
+
+  return EXIT_SUCCESS;
+}
