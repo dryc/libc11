5bdcacd931438806211052a314b0c17ef075edd3
Arto Bendiken <arto@bendiken.net>
2014-11-14 22:34:29 +0100

    Added trivial test cases for EXIT_FAILURE and EXIT_SUCCESS.

---
+++ test/stdlib/check-macros.c
@@ -0,0 +1,13 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include <assert.h> /* for assert() */
+#include <stdio.h>  /* for printf() */
+#include <stdlib.h> /* for EXIT_SUCCESS */
+
+int
+main(void) {
+  (void)EXIT_FAILURE;
+  (void)EXIT_SUCCESS;
+
+  return EXIT_SUCCESS;
+}
