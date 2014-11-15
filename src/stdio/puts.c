52214d70e9046553352cb54428d8238ac0eaa9de
Arto Bendiken <arto@bendiken.net>
2014-11-15 02:00:22 +0100

    Implemented the <stdio.h> puts() function.

--- src/stdio/puts.c
+++ src/stdio/puts.c
@@ -4,7 +4,11 @@
 #include <config.h>
 #endif
 
-#include <stdio.h>
+#include <errno.h>   /* for errno */
+#include <stdio.h>   /* for puts() */
+#include <string.h>  /* for strlen() */
+
+#include "syscall.h" /* for __sys_*() */
 
 /**
  * @date   2014-11-14
@@ -13,5 +17,20 @@
  */
 int
 puts(const char* str) {
-  return (void)str, EOF; // TODO
+  // TODO: reimplement this to be based on fputs() and fputc().
+  long rc;
+
+  rc = __sys_write(1 /*stdout*/, str, strlen(str));
+  if (rc < 0) {
+    errno = -rc;
+    return EOF;
+  }
+
+  rc = __sys_write(1 /*stdout*/, "\n", 1);
+  if (rc < 0) {
+    errno = -rc;
+    return EOF;
+  }
+
+  return 0; /* a nonnegative return value indicates success */
 }
