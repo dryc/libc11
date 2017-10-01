1343371f366049f91004adb4d0e88a73c2cc1d29
Samuel Sarle <samuelsarle@gmail.com>
2017-10-01 19:21:06 +0000

    Added pow function to math.

---
+++ src/math/pow.c
@@ -0,0 +1,32 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+/**
+ * @date   2017-10-01
+ * @author Samuel Sarle
+ * @see    http://libc11.org/math/pow.html
+ */
+
+double
+pow(double x, double y) {
+    double z = 1;
+    int i;
+    for (i=0;i<y;i++)
+        z *= x;
+    return z;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
